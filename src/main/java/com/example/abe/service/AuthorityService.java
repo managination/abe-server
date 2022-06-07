b20aeea45908d397c085941e24f223a5fc3e3ebe
rchekmarov <roman.chekmarov@jetsoftpro.com>
2022-06-07 13:49:55 +0300

    add attribute validation to updateAuthority method

--- src/main/java/com/example/abe/service/AuthorityService.java
+++ src/main/java/com/example/abe/service/AuthorityService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.example.abe.dcpabe.other.GlobalParameters.gp;
 
@@ -39,10 +40,7 @@ public class AuthorityService {
         authorityRepository.save(authority);
         Long authorityId = authorityRepository.findAuthorityByName(body.getName())
                 .orElseThrow(() -> new IllegalStateException("authority was not saved")).getId();
-        String[] attributesWithId = Arrays.stream(body.getAttributes())
-                        .map(attribute -> authorityId.toString() + "_" + attribute)
-                .toArray(String[]::new);
-        updateAuthority(authorityId, body.getName(), attributesWithId);
+        updateAuthority(authorityId, body.getName(), body.getAttributes());
 
     }
 
@@ -77,17 +75,27 @@ public class AuthorityService {
         AuthorityKeys authority = authorityRepository.findById(authorityId)
                 .orElseThrow(() -> new IllegalStateException(
                         ("authority with id " + authorityId + " does not exist")));
-
+        if (attributes == null || attributes.length == 0) {
+            throw new IllegalStateException("attribute was not defined");
+        }
         if (name != null && name.length() > 0 && !Objects.equals(authority.getAuthorityName(), name)) {
             authority.setName(name);
         }
 
-        if (attributes != null && attributes.length > 0 &&
-                !Arrays.equals(authority.getPublicKeys().keySet().toArray(), attributes)) {
-            DCPABE.authorityUpdate(authority, gp, attributes);
-            authorityRepository.save(authority);
-            publicKeysService.addPublicKeys(authority); //add authority PKs to all PKs
+        String[] attributesWithId = Arrays.stream(attributes)
+                .map(attribute -> authorityId + "_" + attribute)
+                .toArray(String[]::new);
+        boolean attributeRepeated = Arrays.stream(attributesWithId)
+                .map(attribute -> {
+                    return authority.getPublicKeys().containsKey(attribute);
+                        }
+                ).collect(Collectors.toList()).contains(true);
+        if (attributeRepeated) {
+            throw new IllegalStateException("one of attribute already exists");
         }
+        DCPABE.authorityUpdate(authority, gp, attributesWithId);
+        authorityRepository.save(authority);
+        publicKeysService.addPublicKeys(authority); //add authority PKs to all PKs
     }
 
     public AuthorityKeys AuthorityKeysByName(String authorityName) {
